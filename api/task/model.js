4c6f18a99ffcd20ead5924224f670461f6e8afed
Samuel Sanchez <wearycodes@gmail.com>
2024-05-20 23:02:38 -0500

    added task stuff

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,29 @@
 // build your `Task` model here
+// task_id - primary key
+// task_description - required
+// task_notes - optional
+// task_completed - the database defaults it to false (integer 0) if not provided
+// project_id - required and points to an actual project_id in the projects table
+
+const db = require("../../data/dbConfig.js");
+
+async function getAll() {
+  const tasks = await db("tasks as t");
+  return tasks;
+}
+
+async function findById(id) {
+  const task = await db("tasks as t").where({ "t.task_id": id }).first();
+  return task;
+}
+
+async function create(task) {
+  const [id] = await db("tasks").insert(task);
+  return findById(id);
+}
+
+module.exports = {
+  getAll,
+  findById,
+  create,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,28 @@
 // build your `/api/tasks` router here
+const router = require("express").Router();
+const Tasks = require("./model.js");
+
+router.get("/", (req, res) => {
+  Tasks.find()
+    .then((Tasks) => {
+      res.status(200).json(Tasks);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to get Tasks", error: err });
+    });
+});
+
+router.post("/", (req, res) => {
+  const task = req.body;
+  Tasks.create(task)
+    .then((newTask) => {
+      res.status(201).json(newTask);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: "Failed to create new Task", error: err });
+    });
+});
+
+module.exports = router;
